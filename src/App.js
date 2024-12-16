36063fa58441573a3ff8ad2d1ba36ee83b22f63f
FabianJoseBlanco <fabians_blanco@hotmail.com>
2024-12-16 08:27:56 -0500

    16/12/2024

---
+++ src/App.js
@@ -0,0 +1,21 @@
+//import logo from "./logo.svg";
+import "./App.css";
+
+function Header() {
+  return <h1>Hello World</h1>;
+}
+
+function Footer() {
+  return <h1>Adios</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
